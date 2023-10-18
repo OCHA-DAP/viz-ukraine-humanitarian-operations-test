e996dec26b278fb3f2a992eb4c16b399bcaa8ae9
danmihaila <danmihaila@users.noreply.github.com>
2023-10-18 11:37:39 +0000

    Deploying to gh-pages from @ OCHA-DAP/viz-ukraine-humanitarian-operations-test@7a36718a3dfadc8fc373f3ae1c31600cffc6f61b 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -1585,7 +1585,7 @@ $( document ).ready(function() {
   function getData() {
     console.log('Loading data...')
     Promise.all([
-      d3.json('https://raw.githubusercontent.com/OCHA-DAP/hdx-scraper-ukraine-viz/a508571cdc2b0b42984e32787add376012a7ef3b/all.json'),
+      d3.json('https://raw.githubusercontent.com/OCHA-DAP/hdx-scraper-ukraine-viz/0e2fec14c31900235244ba34d433856349eea9e1/all.json'),
       d3.json('https://raw.githubusercontent.com/OCHA-DAP/hdx-scraper-ukraine-viz/main/UKR_Border_Crossings.geojson'),
       d3.json('data/ee-regions-bbox.geojson'),
       d3.json('data/ukr_refugee_lines.geojson'),
